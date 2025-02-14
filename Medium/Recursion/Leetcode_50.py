12d2fcbceee2aead8db0f887ab5e7ba221173946
Tushig Erdenebulgan <tushiggod@gmail.com>
2025-02-13 20:24:21 -0600

    Daily Leetcode

--- Medium/Recursion/Leetcode_50.py
+++ Medium/Recursion/Leetcode_50.py
@@ -16,12 +16,22 @@ def myPow(x, n):
         :type n: int
         :rtype: float
         """
-        if n==0:
-            return 1
-        if n<0:
-            return 1/myPow(x,-n)
-        half=myPow(x,n//2)
-        return half*half if n%2==0 else half*half*x
+        def helper(x,n):
+            if n==0:
+                return 1
+            if x==0:
+                return 0
+            result=helper(x,n//2)
+            result=result*result
+            if n%2:
+                return result*x
+            else:
+                return result
+        result=helper(x,abs(n)) 
+        if n>0:
+            return result
+        else:
+            return 1/result
         
         
         
--- Medium/String/Leetcode_43.py
+++ Medium/String/Leetcode_43.py
@@ -36,4 +36,6 @@ def multiply(num1, num2):
 num1 = "123"
 num2 = "456"
 
+
+
 print(multiply(num1,num2))
