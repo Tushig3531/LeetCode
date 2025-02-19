6f5cc4da46a0cdbcc9e207c9afa68e2f6a896976
Tushig Erdenebulgan <tushiggod@gmail.com>
2025-02-18 22:05:18 -0600

    Minor Detail

--- Medium/Backtracking/Leetcode_1415.py
+++ Medium/Backtracking/Leetcode_1415.py
@@ -23,27 +23,27 @@ def getHappyString(n, k):
         :type k: int
         :rtype: str
         """
-        result=[]
-        input_char="abc"
-        if n<=0:
+        result=[] #init result
+        input_char="abc" #base chars
+        if n<=0: #n out of range
             return ""
         
-        def backtrack(combination):
-            if len(combination)==n:
-                result.append("".join(combination))
+        def backtrack(combination): #I am only using combination because I only need the combinations of the char, and if it is not repeating I do not need to check the start, it can start from everywhere 
+            if len(combination)==n: #if the item in the combination touch the limit of n
+                result.append("".join(combination)) #append
                 return
          
-            for char in input_char:
-                if combination and combination[-1]==char:
-                    continue
-                combination.append(char)
-                backtrack(combination)
-                combination.pop()
-        backtrack([])
-        if k<=len(result):
-            return result[k-1]
+            for char in input_char: #looping in the char
+                if combination and combination[-1]==char: #if the appending char and previous char are same
+                    continue #do not include it
+                combination.append(char) #append the char in the combination
+                backtrack(combination) #recursively calling
+                combination.pop() #popping the overflow
+        backtrack([]) #init 
+        if k<=len(result): #because I am appending char in result for n amount, if the length of bigger than k. Which passing our condition.
+            return result[k-1] #we should return the k'th item
         else:
-            return ""
+            return "" #else empty
                     
                     
                         
