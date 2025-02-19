0d45f42c567a775f30ddbfec82113f5f2c941303
Tushig Erdenebulgan <tushiggod@gmail.com>
2025-02-18 21:58:34 -0600

    Daily Leetcode

---
+++ Medium/Backtracking/Leetcode_1415.py
@@ -0,0 +1,55 @@
+# A happy string is a string that:
+# consists only of letters of the set ['a', 'b', 'c'].
+# s[i] != s[i + 1] for all values of i from 1 to s.length - 1 (string is 1-indexed).
+# For example, strings "abc", "ac", "b" and "abcbabcbcb" are all happy strings and strings "aa", "baa" and "ababbc" are not happy strings.
+# Given two integers n and k, consider a list of all happy strings of length n sorted in lexicographical order.
+# Return the kth string of this list or return an empty string if there are less than k happy strings of length n.
+# Example 1:
+# Input: n = 1, k = 3
+# Output: "c"
+# Explanation: The list ["a", "b", "c"] contains all happy strings of length 1. The third string is "c".
+# Example 2:
+# Input: n = 1, k = 4
+# Output: ""
+# Explanation: There are only 3 happy strings of length 1.
+# Example 3:
+# Input: n = 3, k = 9
+# Output: "cab"
+# Explanation: There are 12 different happy string of length 3 ["aba", "abc", "aca", "acb", "bab", "bac", "bca", "bcb", "cab", "cac", "cba", "cbc"]. You will find the 9th string = "cab"
+
+def getHappyString(n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: str
+        """
+        result=[]
+        input_char="abc"
+        if n<=0:
+            return ""
+        
+        def backtrack(combination):
+            if len(combination)==n:
+                result.append("".join(combination))
+                return
+         
+            for char in input_char:
+                if combination and combination[-1]==char:
+                    continue
+                combination.append(char)
+                backtrack(combination)
+                combination.pop()
+        backtrack([])
+        if k<=len(result):
+            return result[k-1]
+        else:
+            return ""
+                    
+                    
+                        
+                 
+            
+n = 1
+k = 4
+
+print(getHappyString(n,k))
