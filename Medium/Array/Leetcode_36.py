68de17352f96c373d8957bc744cfb52d4dc75f81
Tushig Erdenebulgan <tushiggod@gmail.com>
2025-04-20 05:02:29 -0500

    Daily Leetcode

---
+++ Medium/Array/Leetcode_36.py
@@ -0,0 +1,71 @@
+# Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+# Each row must contain the digits 1-9 without repetition.
+# Each column must contain the digits 1-9 without repetition.
+# Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+# Note:
+
+# A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+# Only the filled cells need to be validated according to the mentioned rules.
+ 
+
+# Example 1:
+# Input: board = 
+# [["5","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ,["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]
+# Output: true
+# Example 2:
+# Input: board = 
+# [["8","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ,["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]
+# Output: false
+# Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+def isValidSudoku(board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        def valid(row,column,character):
+            for j in range(9):
+                if j!=column and board[row][j]==character:
+                    return False
+            for i in range(9):
+                if i!=row and board[i][column]==character:
+                    return False
+            box_row_start=(row//3)*3
+            box_column_start=(column//3)*3
+            
+            for i in range(box_row_start,box_row_start+3):
+                for j in range(box_column_start,box_column_start+3):
+                    if (i!=row or j!=column) and board[i][j]==character:
+                        return False
+            return True
+        for i in range(9):
+            for j in range(9):
+                if board[i][j]!=".":
+                    if not valid(i,j,board[i][j]):
+                        return False
+        return True
+                    
+            
+        
+        
+        
+board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+
+print(isValidSudoku(board))
