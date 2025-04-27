0984f842c557ad1ab89578f402268bb42de20f50
Tushig Erdenebulgan <tushiggod@gmail.com>
2025-04-27 10:13:39 -0500

    Daily Leetcode

---
+++ Easy/Array/Leetcode_594.py
@@ -0,0 +1,40 @@
+# We define a harmonious array as an array where the difference between its maximum value and its minimum value is exactly 1.
+# Given an integer array nums, return the length of its longest harmonious subsequence among all its possible subsequences.
+# Example 1:
+# Input: nums = [1,3,2,2,5,2,3,7]
+# Output: 5
+# Explanation:
+# The longest harmonious subsequence is [3,2,2,2,3].
+# Example 2:
+# Input: nums = [1,2,3,4]
+# Output: 2
+# Explanation:
+# The longest harmonious subsequences are [1,2], [2,3], and [3,4], all of which have a length of 2.
+# Example 3:
+# Input: nums = [1,1,1,1]
+# Output: 0
+# Explanation:
+# No harmonic subsequence exists.
+
+def findLHS(nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        hashmap={}
+        for num in nums:
+            hashmap[num]=hashmap.get(num,0)+1
+        # return hashmap        
+        result=0
+        for num, count in hashmap.items():
+        # print(hashmap.items())
+            if num+1 in hashmap:
+                total=count+hashmap[num+1]
+                if total>result:
+                    result=total
+        return result
+            
+        
+nums = [1,3,2,2,5,2,3,7]
+print(findLHS(nums))
+        
